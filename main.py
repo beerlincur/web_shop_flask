7850805fa2d10eabdf5ffd8a9be2a8a4060293eb
DevilJack <grigorysmorodnikov253@mail.ru>
2020-09-11 22:28:22 +0300

    web shop

---
+++ main.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+from cloudipsp import Api, Checkout
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///zhozh_shop.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    isActive = db.Column(db.Boolean, default=True)
+    # text = title = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return "Запись"
+
+
+@app.route('/')
+def index():
+    items = Item.query.order_by(Item.price).all()
+    return render_template('index.html', data=items)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/buy/<int:id>')
+def item_buy(id):
+    item = Item.query.get(id)
+
+    api = Api(merchant_id=1396424,
+              secret_key='test')
+    checkout = Checkout(api=api)
+    data = {
+        "currency": "RUB",
+        "amount": str(item.price) + "00"
+    }
+
+    url = checkout.url(data).get("checkout_url")
+
+    return redirect(url)
+
+
+@app.route('/create', methods=['POST', 'GET'])
+def create():
+    if request.method == "POST":
+        title = request.form['title']
+        price = request.form['price']
+
+        item = Item(title=title, price=price)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Получилась ошибка"
+    else:
+        return render_template('create.html')
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
